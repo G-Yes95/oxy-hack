11549e58e737cd4f28aea95802c894a08d34d792
Antonio Furtado <antoniordf@hotmail.com>
2023-09-22 23:26:47 -0400

    Created the PercentageMath and WadRayMath contracts and the initial structure for the interest rate strategy contract

---
+++ src/InterestRateStrategy.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {LendingPool} from "./LendingPool.sol";
+import {WadRayMath} from "./WadRayMath.sol";
+import {PercentageMath} from "./PercentageMath.sol";
+
+contract InterestRateStrategy {}
