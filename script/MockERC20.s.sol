8bec8f0ae62653ec1b0f9de29073fbe20ba653e4
G-Yes95 <g-yes95@protonmail.com>
2023-09-23 02:38:13 -0400

    added script for deploying mock token

---
+++ script/MockERC20.s.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "../src/mocks/MockERC20.sol";
+
+contract MockERC20Deployer is Script {
+    function setUp() public {}
+
+    function run() public {
+
+        vm.startBroadcast();
+        MockERC20 hETH = new MockERC20("hETH", "hackETH", 18);
+        vm.stopBroadcast();
+        console2.log(address(hETH), "deployed mock token");
+    }
+}
---
+++ script/README.md
@@ -0,0 +1,3 @@
+# Using Scripts 
+
+forge script script/MockERC20.s.sol --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY_PERSONAL --broadcast --verify --watch --etherscan-api-key 4DZZ49ARAJ8SXIC42GCWG3DF1WEEIJNQEI  -vvvv
