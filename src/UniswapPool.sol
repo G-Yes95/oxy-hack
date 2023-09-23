46d0ee0e9f4fec5960b15fbc525cd01ebbd622a7
Antonio Furtado <antoniordf@hotmail.com>
2023-09-23 09:39:16 -0400

    Update to the Uniswap v4 initial implementation following what is done in a tutorial

--- src/UniswapPool.sol
+++ src/UniswapPool.sol
@@ -1,66 +1,84 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
-import "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
-import "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {BaseHook} from "@uniswap/v4-periphery/contracts/BaseHook.sol";
+
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Currency.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+error SwapExpired();
+error OnlyPoolManager();
+
+using CurrencyLibrary for Currency;
+using SafeERC20 for IERC20;
 
 contract UniswapPool {
-    address private _owner;
     IPoolManager public poolManager;
-    IHooks public hooks;
-    IERC20 public tokenA;
-    IERC20 public tokenB;
-    PoolKey public poolKey;
 
-    event PoolInitialized(PoolKey poolKey, int24 tick);
-    event LiquidityModified(
-        int24 tickLower,
-        int24 tickUpper,
-        int256 liquidityDelta
-    );
-
-    constructor(
-        address _poolManager,
-        address _hooks,
-        address _tokenA,
-        address _tokenB
-    ) {
-        _owner = msg.sender;
-        poolManager = IPoolManager(_poolManager);
-        hooks = IHooks(_hooks);
-        tokenA = IERC20(_tokenA);
-        tokenB = IERC20(_tokenB);
+    constructor(IPoolManager _poolManager) {
+        poolManager = _poolManager;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Not authorized");
-        _;
+    function swapTokens(
+        PoolKey calldata poolKey,
+        IPoolManager.SwapParams calldata swapParams,
+        uint256 deadline
+    ) public payable {
+        poolManager.lock(abi.encode(poolKey, swapParams, deadline));
     }
 
-    function initializePool(
-        uint160 sqrtPriceX96,
-        bytes calldata hookData
-    ) external onlyOwner {
-        poolKey = PoolKey({currency0: tokenA, currency1: tokenB, fee: 3000});
+    function lockAcquired(
+        uint256,
+        bytes calldata data
+    ) external returns (bytes memory) {
+        if (msg.sender == address(poolManager)) {
+            revert OnlyPoolManager();
+        }
 
-        int24 tick = poolManager.initialize(poolKey, sqrtPriceX96, hookData);
-        emit PoolInitialized(poolKey, tick);
+        (
+            PoolKey memory poolKey,
+            IPoolManager.SwapParams memory swapParams,
+            uint256 deadline
+        ) = abi.decode(data, (PoolKey, IPoolManager.SwapParams, uint256));
+
+        if (block.timestamp > deadline) {
+            revert SwapExpired();
+        }
+
+        BalanceDelta delta = poolManager.swap(poolKey, swapParams, hookData);
+
+        _settleCurrencyBalance(poolKey.currency0, delta.amount0());
+        _settleCurrencyBalance(poolKey.currency1, delta.amount1());
+
+        return new bytes(0);
     }
 
-    function modifyLiquidity(
-        IPoolManager.ModifyPositionParams memory params,
-        bytes calldata hookData
-    ) external {
-        poolManager.modifyPosition(poolKey, params, hookData);
-        emit LiquidityModified(
-            params.tickLower,
-            params.tickUpper,
-            params.liquidityDelta
+    function _settleCurrencyBalance(
+        Currency currency,
+        int128 deltaAmount
+    ) private {
+        if (deltaAmount < 0) {
+            poolManager.take(currency, msg.sender, uint128(-deltaAmount));
+            return;
+        }
+
+        if (currency.isNative()) {
+            poolManager.settle{value: uint128(deltaAmount)}(currency);
+            return;
+        }
+
+        IERC20(Currency.unwrap(currency)).safeTransferFrom(
+            msg.sender,
+            address(poolManager),
+            uint128(deltaAmount)
         );
+        poolManager.settle(currency);
     }
-
-    // Additional functions for swapping, donating, etc. can be added here based on the methods available in IPoolManager.
 }
