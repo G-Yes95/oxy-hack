62305e3625bfc578ad4f48eece630ff6261f4001
Antonio Furtado <antoniordf@hotmail.com>
2023-09-22 21:23:47 -0400

    Installed openzeppelin, created remappings.txt and added imports and basic contract outline.

---
+++ src/LendingPool.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// imports
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract LendingPool is
+    ERC20("PoolToken", "PT"),
+    ReentrancyGuard,
+    OwnableUpgradeable
+{}
