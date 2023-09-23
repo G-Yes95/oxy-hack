c402745dd9460607393f55714a4859439265b9c5
G-Yes95 <g-yes95@protonmail.com>
2023-09-22 21:37:19 -0400

    setup remappings

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Counter is ERC20 {
     uint256 public number;
+
+     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
