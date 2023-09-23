8b00cb6ada60fe58ed96c67302899983425faa10
Le <leyu00@gmail.com>
2023-09-23 17:39:56 -0400

    Add setter, remove from constructor

--- src/accountingTokens/DebtToken.sol
+++ src/accountingTokens/DebtToken.sol
@@ -8,13 +8,11 @@ contract DebtToken is ERC1155 {
     error ER1155NonApprovedMinter();
     error ER1155NonApprovedBurner();
 
-    address immutable loanFactory;
+    address loanFactory;
 
     // Used to track the totalSupply for each ID
     mapping(uint256 => uint256) public totalSupply;
-    constructor(address _loanFactory) ERC1155("DEBT TOKEN") {
-        loanFactory = _loanFactory;
-    }
+    constructor() ERC1155("DEBT TOKEN") {}
 
     function mint(address account, uint256 id, uint256 value) public {
         if (_msgSender() != loanFactory) {
@@ -33,5 +31,11 @@ contract DebtToken is ERC1155 {
 
         _burn(account, id, value);
         totalSupply[id] -= value;
+    }
+
+    // !! Setters for easier re-deployments !!
+    // @dev these are unsafe, and are for hackathon use only. Consider adding access control
+    function setLoanFactory(address _loanFactory) external {
+        loanFactory = _loanFactory;
     }
 }
