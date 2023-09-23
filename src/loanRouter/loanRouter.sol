a2277881cf00aec76eb54ebb5eb52d5f68a2f749
G-Yes95 <g-yes95@protonmail.com>
2023-09-23 01:55:39 -0400

    updated loanRouter

--- src/loanRouter/loanRouter.sol
+++ src/loanRouter/loanRouter.sol
@@ -7,14 +7,30 @@ pragma solidity ^0.8.20;
 // iLoanFactory
 
 // imports
-// iButtonToken
+import "@buttonwood-protocol/button-wrappers/contracts/interfaces/IButtonToken.sol";
+import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
 
 contract LoanRouter {
-    function createAndBorrow() public {
+
+    mapping(address => address) public buttonMapping;
+
+    constructor(address[] memory _rawCollateral, address[] memory _buttonToken) {
+        buttonMapping[_rawCollateral[0]] = _buttonToken[0];
+        buttonMapping[_rawCollateral[1]] = _buttonToken[1];
+    }
+
+    function createAndBorrow(address _rawCollateral, address _lendingPool, uint256 _amount) public {
         // transfer collateralTokens to this contract
-        // button up the collateralTokens
-        // call approve on buttonTokens
+        TransferHelper.safeTransfer(_rawCollateral, address(this), _amount); 
+        // approve collateral to be buttoned 
+        TransferHelper.safeApprove(_rawCollateral, buttonMapping[_rawCollateral], _amount);
+  
         // call create on loanFactory
+
+        // button up the collateralTokens into the new loan 
+        // TODO: Replace msg.sender with new loanContract address
+        IButtonToken(buttonMapping[_rawCollateral]).mintFor(msg.sender, _amount);
+
         // call borrow on LendingPool
     }
 
@@ -26,7 +42,6 @@ contract LoanRouter {
     function repayAndCollect() public {
         // transfer stablecoins to this contract
         // approve stablecoins to be spent by loanContract
-        // transfer ERC1155 debtTokens?
         // call repay on loanContract
         // call collect on lendingPool
         // unwrap collateral to borrower
