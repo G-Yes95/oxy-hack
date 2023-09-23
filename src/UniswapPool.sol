84e017a69651817b2344d584979bb78ea382f529
Antonio Furtado <antoniordf@hotmail.com>
2023-09-23 07:34:14 -0400

    updated initial template for the pool

--- src/UniswapPool.sol
+++ src/UniswapPool.sol
@@ -1,56 +1,66 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
 
-import "@uniswap/v4-core/contracts/interfaces/IUniswapV4Factory.sol";
-import "@uniswap/v4-core/contracts/interfaces/IUniswapV4Pool.sol";
-import "@uniswap/v4-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
+import "@uniswap/v4-core/contracts/types/PoolKey.sol";
 
 contract UniswapPool {
     address private _owner;
-    IUniswapV4Factory public uniswapV4Factory;
-    ISwapRouter public uniswapV4Router;
+    IPoolManager public poolManager;
+    IHooks public hooks;
     IERC20 public tokenA;
     IERC20 public tokenB;
-    IUniswapV4Pool public pool;
-    uint24 public constant POOL_FEE = 3000; // 0.3% fee
+    PoolKey public poolKey;
+
+    event PoolInitialized(PoolKey poolKey, int24 tick);
+    event LiquidityModified(
+        int24 tickLower,
+        int24 tickUpper,
+        int256 liquidityDelta
+    );
 
     constructor(
-        address _uniswapV4Factory,
-        address _uniswapV4Router,
+        address _poolManager,
+        address _hooks,
         address _tokenA,
         address _tokenB
     ) {
         _owner = msg.sender;
-        uniswapV4Factory = IUniswapV4Factory(_uniswapV4Factory);
-        uniswapV4Router = ISwapRouter(_uniswapV4Router);
+        poolManager = IPoolManager(_poolManager);
+        hooks = IHooks(_hooks);
         tokenA = IERC20(_tokenA);
         tokenB = IERC20(_tokenB);
     }
 
-    function createPool() external {
-        require(msg.sender == owner, "Not authorized");
-        uniswapV4Factory.createPool(address(tokenA), address(tokenB), poolFee);
-        pool = IUniswapV4Pool(
-            uniswapV4Factory.getPool(address(tokenA), address(tokenB), poolFee)
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Not authorized");
+        _;
+    }
+
+    function initializePool(
+        uint160 sqrtPriceX96,
+        bytes calldata hookData
+    ) external onlyOwner {
+        poolKey = PoolKey({currency0: tokenA, currency1: tokenB, fee: 3000});
+
+        int24 tick = poolManager.initialize(poolKey, sqrtPriceX96, hookData);
+        emit PoolInitialized(poolKey, tick);
+    }
+
+    function modifyLiquidity(
+        IPoolManager.ModifyPositionParams memory params,
+        bytes calldata hookData
+    ) external {
+        poolManager.modifyPosition(poolKey, params, hookData);
+        emit LiquidityModified(
+            params.tickLower,
+            params.tickUpper,
+            params.liquidityDelta
         );
     }
 
-    function addLiquidityInRange(
-        uint256 amountA,
-        uint256 amountB,
-        int24 tickLower,
-        int24 tickUpper
-    ) external {
-        // Approve the pool to transfer tokens on behalf of the liquidity provider
-        tokenA.approve(address(pool), amountA);
-        tokenB.approve(address(pool), amountB);
-
-        // Parameters for the mint function
-        address recipient = msg.sender;
-        uint128 liquidityAmount = uint128(amountA); // This is a simplification; in reality, you'd calculate the liquidity amount based on the amounts of tokenA and tokenB and the current price.
-
-        // Call the mint function on the pool
-        pool.mint(recipient, tickLower, tickUpper, liquidityAmount, "");
-    }
+    // Additional functions for swapping, donating, etc. can be added here based on the methods available in IPoolManager.
 }
